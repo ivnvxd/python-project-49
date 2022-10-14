0af9ec9263b74c83832daf24f078599878e3b5fd
Andrey Ivanov <veny.xd@gmail.com>
2022-10-14 19:35:43 +0200

    refactor brain-even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -6,8 +6,9 @@ def main():
     # Greet the user and get his name
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
-    print('Answer "yes" if the number is even, otherwise answer "no".')
+
+    print(f'''Hello, {name}!
+Answer "yes" if the number is even, otherwise answer "no".''')
 
     # Count correct answers
     correct_answers = 0
