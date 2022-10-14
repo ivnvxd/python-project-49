b39956e5c0f268eae0777823e217caf0e346412f
Andrey Ivanov <veny.xd@gmail.com>
2022-10-14 20:41:25 +0200

    refactor cognitive compleity

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -7,8 +7,8 @@ def greet():
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
 
-    print(f'''Hello, {name}!
-Answer "yes" if the number is even, otherwise answer "no".''')
+    print(f'Hello, {name}!')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
 
     return name
 
